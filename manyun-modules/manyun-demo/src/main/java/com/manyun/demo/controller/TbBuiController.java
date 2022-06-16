8ea44bfc6f1ca93901f68ccddb3accd55f82f7cb
yanwei <forcontinue@163.com>
2022-06-16 11:39:36 +0800

    1. 网关动态配置 限流服务整合完善 2. demo模块注入演示

--- manyun-modules/manyun-demo/src/main/java/com/manyun/demo/controller/TbBuiController.java
+++ manyun-modules/manyun-demo/src/main/java/com/manyun/demo/controller/TbBuiController.java
@@ -1,8 +1,12 @@
 package com.manyun.demo.controller;
 
 
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.manyun.common.core.domain.R;
+import com.manyun.demo.domain.TbBui;
+import com.manyun.demo.service.ITbBuiService;
 import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -21,6 +25,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/demo/tbBui")
 @Api("业务模块相关apis(web)")
 public class TbBuiController {
+
+    @Autowired
+    ITbBuiService buiService;
 
     @GetMapping("/list")
     public R list(){
