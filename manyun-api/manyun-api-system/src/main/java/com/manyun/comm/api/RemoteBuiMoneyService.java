f5a4ccd536dab12f7336ef45adc736951f69a6d3
qinxiaohui <919224501@qq.com>
2022-08-18 20:52:44 +0800

    实名接口修改

--- manyun-api/manyun-api-system/src/main/java/com/manyun/comm/api/RemoteBuiMoneyService.java
+++ manyun-api/manyun-api-system/src/main/java/com/manyun/comm/api/RemoteBuiMoneyService.java
@@ -20,7 +20,7 @@ public interface RemoteBuiMoneyService {
     @PostMapping("/money/updateUserMoney")
      R updateUserMoney(@RequestBody UserRealMoneyForm userRealMoneyForm, @RequestHeader(SecurityConstants.FROM_SOURCE) String source);
 
-    @GetMapping("/money/checkIdentity")
+    @GetMapping("/money/checkIdentity/{identityNo}")
     R checkIdentity(@PathVariable("identityNo") String identityNo, @RequestHeader(SecurityConstants.FROM_SOURCE) String source);
 
 }
