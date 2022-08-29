a720332a796507e76a576c7a23bce19a68c5a599
yanwei <forcontinue@163.com>
2022-08-29 11:23:11 +0800

    表单验证修复

--- manyun-modules/manyun-business/src/main/java/com/manyun/business/domain/form/OrderPayForm.java
+++ manyun-modules/manyun-business/src/main/java/com/manyun/business/domain/form/OrderPayForm.java
@@ -18,8 +18,8 @@ public class OrderPayForm implements Serializable {
     private String orderId;
 
 
-    @ApiModelProperty(value = "支付类型,1=微信,2=支付宝,0=余额支付，3=银联",required = true)
-    @Range(min = 0,max = 3,message = "支付类型错误")
+    @ApiModelProperty(value = "支付类型,1=微信,2=支付宝,0=余额支付，3=银联，4=杉德支付",required = true)
+    @Range(min = 0,max = 4,message = "支付类型错误")
     private Integer payType;
 
 
