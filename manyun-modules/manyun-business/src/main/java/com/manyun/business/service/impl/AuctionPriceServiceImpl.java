d116809ba56916b9a3491ccbfe386c646dfd9678
qinxiaohui <919224501@qq.com>
2022-08-31 15:07:18 +0800

    返回余额支付的部分

--- manyun-modules/manyun-business/src/main/java/com/manyun/business/service/impl/AuctionPriceServiceImpl.java
+++ manyun-modules/manyun-business/src/main/java/com/manyun/business/service/impl/AuctionPriceServiceImpl.java
@@ -336,6 +336,11 @@ public class AuctionPriceServiceImpl extends ServiceImpl<AuctionPriceMapper, Auc
         if (auctionPrice.getEndPayTime() != null) {
             myAuctionPriceVo.setEndPayTime(auctionPrice.getEndPayTime());
         }
+        AuctionOrder auctionOrder = auctionOrderService.getOne(Wrappers.<AuctionOrder>lambdaQuery().eq(AuctionOrder::getSendAuctionId, auctionPrice.getAuctionSendId())
+                .eq(AuctionOrder::getToUserId, auctionPrice.getUserId()));
+        if (auctionOrder != null) {
+            myAuctionPriceVo.setMoneyBln(auctionOrder.getMoneyBln());
+        }
         String mediaUrl = "";
         if (auctionSend.getGoodsType() == 1) {
             mediaUrl = mediaService.initMediaVos(auctionSend.getGoodsId(), BusinessConstants.ModelTypeConstant.COLLECTION_MODEL_TYPE).get(0).getMediaUrl();
