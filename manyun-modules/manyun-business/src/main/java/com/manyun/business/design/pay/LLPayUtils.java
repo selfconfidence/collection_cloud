ac00ec2340fb2170eecdf25fbafa2ac5825b5753
gaofenglei <15036380340@163.com>
2022-09-16 17:56:28 +0800

    连连开户 充值资金流水

--- manyun-modules/manyun-business/src/main/java/com/manyun/business/design/pay/LLPayUtils.java
+++ manyun-modules/manyun-business/src/main/java/com/manyun/business/design/pay/LLPayUtils.java
@@ -260,7 +260,7 @@ public class LLPayUtils {
         匿名用户：ANONYMOUS
          */
         params.setUser_type("REGISTERED");
-        params.setNotify_url(LianLianPayEnum.BIND_CARD_APPLY.getNotifyUrl());
+        params.setNotify_url(LianLianPayEnum.USER_TOPUP.getNotifyUrl());
         params.setReturn_url(returnUrl);
         // 交易发起渠道设置
         params.setFlag_chnl("H5");
