1227a0bc77fb8a175d8dd04ba2ca91aa90367c6b
qinxiaohui <919224501@qq.com>
2022-09-20 21:25:58 +0800

    时间修改

--- manyun-modules/manyun-business/src/main/java/com/manyun/business/domain/vo/SyntheticActivityVo.java
+++ manyun-modules/manyun-business/src/main/java/com/manyun/business/domain/vo/SyntheticActivityVo.java
@@ -23,11 +23,11 @@ public class SyntheticActivityVo {
     @ApiModelProperty("活动状态;1=待开始,2=进行中,3=结束")
     private Integer actionStatus;
 
-    @JsonFormat(locale = "zh", timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(locale = "zh", timezone = "GMT+8", pattern = "yyyy-MM-dd")
     @ApiModelProperty("活动开始时间")
     private LocalDateTime startTime;
 
-    @JsonFormat(locale = "zh", timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(locale = "zh", timezone = "GMT+8", pattern = "yyyy-MM-dd")
     @ApiModelProperty("活动结束时间")
     private LocalDateTime endTime;
 
