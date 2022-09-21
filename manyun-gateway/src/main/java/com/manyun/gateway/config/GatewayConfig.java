f13ef0d0b01fd6230059e4ca3b0121a14d32466f
yanwei <forcontinue@163.com>
2022-09-21 10:19:49 +0800

    网关兼容升级

--- manyun-gateway/src/main/java/com/manyun/gateway/config/GatewayConfig.java
+++ manyun-gateway/src/main/java/com/manyun/gateway/config/GatewayConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import com.manyun.gateway.handler.SentinelFallbackHandler;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.http.codec.support.DefaultServerCodecConfigurer;
 
 /**
  * 网关限流配置
