2ac4f3eadd62bba3ea668e1b691e2ba9c67908ee
qinxiaohui <919224501@qq.com>
2022-11-05 09:27:31 +0800

    杉德云账户相关

--- manyun-modules/manyun-business/src/main/java/com/manyun/business/service/impl/OrderServiceImpl.java
+++ manyun-modules/manyun-business/src/main/java/com/manyun/business/service/impl/OrderServiceImpl.java
@@ -41,6 +41,7 @@ import com.manyun.common.mq.producers.msg.DeliverMsg;
 import com.manyun.common.redis.service.BuiCronService;
 import com.manyun.common.redis.service.RedisService;
 import com.manyun.common.security.utils.SecurityUtils;
+import io.swagger.models.auth.In;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -530,6 +531,7 @@ public class OrderServiceImpl extends ServiceImpl<OrderMapper, Order> implements
     @Transactional(rollbackFor = Exception.class)
     ///ShandePayEnum.COLLECTION_BOX_SHANDE_PAY.setReturnUrl(orderPayForm.getReturnUrl())
     public PayVo unifiedOrder(OrderPayForm orderPayForm,String userId) {
+        Assert.isTrue(Integer.valueOf(6).equals(orderPayForm.getPayType()) && moneyService.checkSandAccountStatus(userId), "暂未开通杉德云账户");
         Order order = getById(orderPayForm.getOrderId());
         Assert.isFalse(Integer.valueOf(5).equals(orderPayForm.getPayType()) && order.getMoneyBln().compareTo(NumberUtil.add(0D)) >0, "当前状态暂不支持此支付方式");
         checkUnified(order,userId,orderPayForm.getPayPass(),orderPayForm.getPayType());
